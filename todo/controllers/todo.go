c50005a715d3b8cb66ec65ba1afb9706c754514f
unknown <nhd7noushadazeez@gmail.com>
2025-01-07 22:09:21 +0530

    todo crud go

--- todo/controllers/todo.go
+++ todo/controllers/todo.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"fmt"
 	"gobasics/todo/config"
 	"gobasics/todo/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // create todo
@@ -23,4 +25,85 @@ func CreateTodo(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, todo)
+}
+
+// get todos
+func GetTodos(c *gin.Context) {
+	var todos []models.Todo
+
+	if result := config.DB.Find(&todos); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"items": fmt.Sprintf("%d items", len(todos)), "data": todos})
+}
+
+// get todo by id
+func GetTodo(c *gin.Context) {
+	id := c.Param("id")
+
+	var todo models.Todo
+
+	if result := config.DB.First(&todo, id); result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found!"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
+}
+
+// update todo
+func UpdateTodo(c *gin.Context) {
+	id := c.Param("id")
+
+	var todo models.Todo
+
+	if result := config.DB.First(&todo, id); result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found!"})
+		}else{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
+		return
+	}
+
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} 
+
+	if result := config.DB.Save(&todo); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
+}
+
+// delete todo
+func DeleteTodo(c *gin.Context) {
+	id := c.Param("id")
+
+	var todo models.Todo
+
+	if result := config.DB.Find(&todo, id); result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found!"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
+		return
+	}
+
+	if result := config.DB.Delete(&todo); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
--- todo/main.go
+++ todo/main.go
@@ -13,6 +13,10 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/create_todo", controllers.CreateTodo)
+	r.GET("/get_todos", controllers.GetTodos)
+	r.GET("/get_todo/:id", controllers.GetTodo)
+    r.PUT("/update_todo/:id", controllers.UpdateTodo)
+    r.DELETE("/delete_todo/:id", controllers.DeleteTodo)
 
 	r.Run(":8000")
 }
