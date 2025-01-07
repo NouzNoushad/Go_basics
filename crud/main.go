fb41750ce9d95412ab08c1168180a8b8c28ada61
unknown <nhd7noushadazeez@gmail.com>
2025-01-07 17:39:17 +0530

    rest-api crud using go

--- crud/main.go
+++ crud/main.go
@@ -48,13 +48,78 @@ func createProduct(c *gin.Context) {
 }
 
 // get products
-func getProducts(c *gin.Context){
-    var products []Product
-    if result := db.Find(&products); result.Error != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
-    }
+func getProducts(c *gin.Context) {
+	var products []Product
+	if result := db.Find(&products); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+	}
 
-    c.JSON(http.StatusOK, products)
+	c.JSON(http.StatusOK, products)
+}
+
+// get product by id
+func getProduct(c *gin.Context) {
+	id := c.Param("id")
+	var product Product
+	if result := db.First(&product, id); result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, product)
+}
+
+// update product
+func updateProduct(c *gin.Context) {
+	id := c.Param("id")
+	var product Product
+
+	if result := db.First(&product, id); result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
+		return
+	}
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if result := db.Save(&product); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, product)
+}
+
+// delete product
+func deleteProduct(c *gin.Context) {
+	id := c.Param("id")
+	var product Product
+
+	if result := db.First(&product, id); result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
+		return
+	}
+
+	if result := db.Delete(&product); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
 
 func main() {
@@ -64,6 +129,9 @@ func main() {
 
 	r.POST("/products", createProduct)
 	r.GET("/products", getProducts)
+	r.GET("/products/:id", getProduct)
+	r.PUT("/products/:id", updateProduct)
+	r.DELETE("/products/:id", deleteProduct)
 
 	r.Run(":8080")
 }
